d554dc49ee9ad717a9f090f95faf70bd30627a7c
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-09-12 09:34:39 -0500

    add missing semester

---
+++ missing_semester/4_data_wrangling/command.sh
@@ -0,0 +1,49 @@
+# Find Words with grep
+#
+# ssh tsp journalctl | grep ssh
+#  - wasteful since transfers all over ssh then greps
+#  - better way ssh tsp 'journalctl | grep ssh | grep "Disconnected from"' | less
+#   - less is a pager 
+#
+# cat file > newfile.log
+#  - outputs file to new file, redirects STDOUT to new file
+# 
+# in the above example, tsp is remote computer connected to
+#
+# SED, stream editor
+# cat ssh.log | sed "s/.*Disconnected from//'
+#  - replace anything before connected from
+#  - ex: Jan 13 1646:56 thesquareplanet.com sshd[24201]: Disconnected from invalid user wp-user 62.162.65.145 port 45788 [preauth]
+#  - becomes invalid user wp-user 62.162.65.145 port 45788 [preauth]
+#
+#  - regex: way to match text
+#    - special chars:
+#      - .: any character
+#      - *: 0/more of any character
+
+echo 'aba' | sed 's/[ab]//'
+# ba, replaces once and done
+echo 'abac' | sed 's/[ab]//g'
+# c, removes all a's and b's
+echo 'abcabaacb' | sed -E 's/(ab)*//g'
+# replace 0/more of string 'ab' --> caacb
+# -E -- modern regular expressiosn otherwise prefix parenth with \
+echo 'abcababcbc' | sed -E 's/(ab|bc)*//g'
+# produces cc --> removes all instances of 'ab' and 'bc'
+
+echo 'Disconnected from invalid user Disconnected from 84.211' | sed 's/.*Disconnected from //'
+# matches are greedy, second Disconnected From matches more with * regex 
+# ? is 0/1, + is many
+# anchoring: ^ (beginning of line), ? (end of line)  -- matches whole line
+# cat ssh.log | sed -E 's/^.*Disconnected from (invalid |authentiating )?user (.*) [0-9.]+ port [0-9]+(\[preauth\]?%/\2/'
+# - () are capture groups, replacement value \2 is capture group
+# capture groups: remember specific value to remember later
+
+# suffix * with ? for non-greedy match
+#
+#
+#
+# | sort, sorts lines
+# | uniq, prints unqiue lines, uniq -c --> count duplicates and list count
+
+
